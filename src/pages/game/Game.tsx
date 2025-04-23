a7e2cbaf722da9782b16d907a227e2b060734ad4
Yakup <yakup.senel33@gmail.com>
2025-04-23 10:28:51 +0200

    Approve / Deny button

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,6 +1,25 @@
+import Approve from "../../components/approve/Approve";
 import "./Game.css";
 
 function Game() {
-	return <></>;
+	return (
+		<>
+			<Approve />
+		</>
+	);
 }
 export default Game;
+
+/*
+Critères de validation
+
+J'ai un bouton accepter ou refuser
+
+
+
+Tests de validation
+
+Je clique sur accepter, le dresseur rentre dans la zone, ajouter du score etc, amener le prochain dresseur
+
+Je clique sur refuser, le dresseur ne rentre pas dans la zone, tout ce qui se passe quand il est refusé
+*/
