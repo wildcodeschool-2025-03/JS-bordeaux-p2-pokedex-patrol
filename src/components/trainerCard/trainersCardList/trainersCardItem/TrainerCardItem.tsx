a9db144b6d801a8e05de82f35611a756f8bbea91
Yakup <yakup.senel33@gmail.com>
2025-05-01 17:17:06 +0200

    removed test code of TrainerCheck and kanto trainer card done

--- src/components/trainerCard/trainersCardList/trainersCardItem/TrainerCardItem.tsx
+++ src/components/trainerCard/trainersCardList/trainersCardItem/TrainerCardItem.tsx
@@ -2,22 +2,24 @@ import "./TrainerCardItem.css";
 
 function TrainerCardItem({ trainerCard }) {
 	return (
-		<div className={`trainer-card region_${trainerCard.trainerRegion}`}>
-			<h1 className="title">{trainerCard.trainerName}</h1>
-			<img
-				className="trainer-image"
-				src={trainerCard.trainerImgCrop}
-				alt="le dresseur"
-			/>
-			<div className="trainer-badges">
-				{trainerCard.trainerBadges.map((badge, key) => (
-					<img
-						className="trainer-badge"
-						key={badge.imgBadge}
-						src={badge.imgBadge}
-						alt="badge"
-					/>
-				))}
+		<div className="modal_overlay">
+			<div className={`trainer_card region_${trainerCard.trainerRegion}`}>
+				<h2 className="trainer_name">{trainerCard.trainerName}</h2>
+				<img
+					className="trainer_image"
+					src={trainerCard.trainerImgCrop}
+					alt="le dresseur"
+				/>
+				<div className="trainer_badges">
+					{trainerCard.trainerBadges.map((badge, key) => (
+						<img
+							className="trainer_badge"
+							key={badge.imgBadge}
+							src={badge.imgBadge}
+							alt="badge"
+						/>
+					))}
+				</div>
 			</div>
 		</div>
 	);
