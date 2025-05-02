308cb6335380309965557774ce7af3a00f079100
Yakup <yakup.senel33@gmail.com>
2025-05-02 12:17:21 +0200

    added last regions

--- src/components/trainerCard/trainersCardList/trainersCardItem/TrainerCardItem.tsx
+++ src/components/trainerCard/trainersCardList/trainersCardItem/TrainerCardItem.tsx
@@ -2,26 +2,32 @@ import "./TrainerCardItem.css";
 
 function TrainerCardItem({ trainerCard }) {
 	return (
-		<div className="modal_overlay">
-			<div className={`trainer_card region_${trainerCard.trainerRegion}`}>
-				<h2 className="trainer_name">{trainerCard.trainerName}</h2>
-				<img
-					className="trainer_image"
-					src={trainerCard.trainerImgCrop}
-					alt="le dresseur"
-				/>
-				<div className="trainer_badges">
-					{trainerCard.trainerBadges.map((badge, key) => (
+		<>
+			{trainerCard ? (
+				<div className="modal_overlay">
+					<div className={`trainer_card region_${trainerCard.trainerRegion}`}>
+						<h2 className="trainer_name">{trainerCard.trainerName}</h2>
 						<img
-							className="trainer_badge"
-							key={badge.imgBadge}
-							src={badge.imgBadge}
-							alt="badge"
+							className="trainer_image"
+							src={trainerCard.trainerImgCrop}
+							alt="le dresseur"
 						/>
-					))}
+						<div className="trainer_badges">
+							{trainerCard.trainerBadges.map((badge, key) => (
+								<img
+									className="trainer_badge"
+									key={badge.imgBadge}
+									src={badge.imgBadge}
+									alt="badge"
+								/>
+							))}
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			) : (
+				alert("fin de partie")
+			)}
+		</>
 	);
 }
 
