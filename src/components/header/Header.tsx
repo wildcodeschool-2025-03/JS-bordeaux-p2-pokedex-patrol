15aaf777e86026bd033ef90087b6e9b63ac7f0d8
Yakup <yakup.senel33@gmail.com>
2025-05-12 14:28:57 +0200

    removed of console.log

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,26 @@
 import "./Header.css";
+import { useState } from "react";
+import "../../pages/game/Game.css";
 
 function Header() {
+	const [activeImage, setActiveImage] = useState(false);
+
+	const handleClick = () => {
+		setActiveImage(true);
+	};
 	return (
 		<header>
 			<img
-				onClick={() => console.log("onClick")}
-				onKeyDown={() => console.log("onKeyDown")}
+				onClick={handleClick}
+				onKeyDown={handleClick}
 				src="src/assets/images/home/speaker.svg"
 				alt="speaker"
 			/>
+			{activeImage && (
+				<div className="information_box">
+					<p>Information à venir</p>
+				</div>
+			)}
 		</header>
 	);
 }
