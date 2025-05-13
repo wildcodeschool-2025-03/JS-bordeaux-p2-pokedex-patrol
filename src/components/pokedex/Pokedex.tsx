079d331a956a2b8a2e26db2d8a0e0f01fc88ebc2
Kevin DeAlmeida <dealmeida.dak@gmail.com>
2025-05-13 13:55:41 +0200

    update PR

--- src/components/pokedex/Pokedex.tsx
+++ src/components/pokedex/Pokedex.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { usePokemonContext } from "../../context/PokemonContext";
-import PokedexModal from "./PokedexModal.tsx";
+import PokedexModal from "./PokedexModale.tsx";
 
 const Pokedex = () => {
 	const { pokemonData } = usePokemonContext();
